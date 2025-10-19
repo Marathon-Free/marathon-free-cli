2882efced32d221d885284101f3662da6f3975fd
ThePJNinja <flameburnface@gmail.com>
2025-10-19 19:39:04 -0400

    Fixed a REALLY dumb error and made the weapon face where you're looking.

--- entities/being/player/player_body_3d.gd
+++ entities/being/player/player_body_3d.gd
@@ -6,6 +6,7 @@ class_name PlayerBody3D extends CharacterBody3D
 @onready var PIVOT_X := $PivotY/PivotX as Node3D
 @onready var CAMERA := $PivotY/PivotX/Camera3D as Camera3D
 @onready var RAY_CAST := $PivotY/PivotX/Camera3D/RayCast3D as RayCast3D
+@onready var VIEW_MODEL := $PivotY/PivotX/Camera3D/ViewModel as ViewModel
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -16,7 +17,13 @@ func _ready() -> void:
 	pass
 	#print("Ready")
 
-func _physics_process(_delta: float) -> void:
-	RAY_CAST.
+func _physics_process(delta: float) -> void:
+	var facing : Vector3
+	if RAY_CAST.is_colliding():
+		facing = RAY_CAST.get_collision_point()
+	else:
+		facing = -1000 * CAMERA.global_transform.basis.z
 	
+	VIEW_MODEL.point_held_item(facing, delta)
+	#print(facing)
 	return
--- entities/being/player/view_model/view_model.gd
+++ entities/being/player/view_model/view_model.gd
@@ -8,14 +8,25 @@ class_name ViewModel extends Node3D
 @export var HELD_ITEM:HeldItem:
 	set(v):
 		HELD_ITEM = v
-		if Engine.is_editor_hint(): load_weapon()
+		if Engine.is_editor_hint(): load_held_item()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_weapon()
+	load_held_item()
 
-func load_weapon() -> void:
+func load_held_item() -> void:
 	MOVEMENT_PIVOT.position = HELD_ITEM.offset
 	MESH_INSTANCE.rotation = HELD_ITEM.rotation
 	MESH_INSTANCE.scale = HELD_ITEM.scale
 	MESH_INSTANCE.mesh = HELD_ITEM.mesh
+
+func point_held_item(global_point: Vector3, delta: float) -> void:
+	var currently_facing := -MOVEMENT_PIVOT.global_transform.basis.z
+	var vector_to := global_point - MOVEMENT_PIVOT.global_position
+	
+	var axis := currently_facing.cross(vector_to).normalized()
+	var angle := currently_facing.angle_to(vector_to)
+	
+	MOVEMENT_PIVOT.global_rotate(axis, lerpf(0.0, angle, 2*delta))
+	MOVEMENT_PIVOT.rotation.z = 0
+	print(MOVEMENT_PIVOT.global_rotation)
